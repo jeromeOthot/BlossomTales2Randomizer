a332f5f069844ee8f4a897a8fce935436e313442
unknown <jerome.othot@gmail.com>
2026-07-15 21:45:55 -0400

    Ajouter le reste du code de la fonction equipShield. Modifier le sprite de l'item obtenu

--- BlossomTales2.Randomizer.mm/Patch_CS_LilysHouse.cs
+++ BlossomTales2.Randomizer.mm/Patch_CS_LilysHouse.cs
@@ -1,23 +1,26 @@
-﻿using System.Numerics;
-
+﻿using Microsoft.Xna.Framework;
+using MonoMod;
+#nullable disable
 namespace BlossomTales2
 {
     class patch_CS_LilysHouse : CS_LilysHouse
     {
+        private Puppet lily = new Puppet("lily", new Vector3(0f, 0.0f, 0f));
+        
         public extern void orig_equipShield();
         
         public void equipShield()
         {
-            //this.lily.play("getItem");
+            lily.play("getItem");
             Game1.playSoundCue("newWeapon");
-            //Game1.player.Position = this.lily.getPosition();
-            //Game1.Particles.Add((Particle) new P_GetItem(this.lily.getPosition() + new Vector3(0.0f, 100f, 0.0f), 47));
-            //Game1.Particles.Add((Particle) new GetItemLight(this.lily.getPosition()));
+            Game1.player.Position = lily.getPosition();
+            Game1.Particles.Add((Particle) new P_GetItem(lily.getPosition() + new Vector3(0.0f, 100f, 0.0f), 48));
+            Game1.Particles.Add((Particle) new GetItemLight(lily.getPosition()));
             Game1.player.Inventory.Add(EquipableItem.ItemList.Guitar);
             //Game1.player.Ability[0] = (EquipableItem) new E_Shield();
             this.tweener.Timer(2.3f).OnComplete((Action) (() =>
             {
-                //this.lily.play("idleDown");
+                lily.play("idleDown");
                 this.tweener.Timer(0.2f).OnComplete(new Action(this.equipSword));
             }));
         }
