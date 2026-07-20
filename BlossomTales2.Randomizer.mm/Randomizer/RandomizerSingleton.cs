845c5f5c30ca7e831cfe97fd382c546162078746
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-07-19 22:05:58 -0400

    Ajouter quelques trésors dans la liste.

--- BlossomTales2.Randomizer.mm/Randomizer/RandomizerSingleton.cs
+++ BlossomTales2.Randomizer.mm/Randomizer/RandomizerSingleton.cs
@@ -32,7 +32,13 @@ namespace BlossomTales2.Randomizer.mm
             _locations = new Dictionary<LocationId, EquipableItem.ItemList>
             {
                 { new LocationId("blossom-lilyHouse.tmx", "npc7", new Vector3(480f, 0f, 328f)), EquipableItem.ItemList.Guitar },
-                { new LocationId("blossom-house2.tmx", "Chest_Small", new Vector3(708f, 0f, 356f)), EquipableItem.ItemList.Bow }
+                { new LocationId("blossom-house1.tmx", "Chest_Small", new Vector3(672f, 0f, 308f)), EquipableItem.ItemList.GoldCoin }, //Random small
+                { new LocationId("blossom-house2.tmx", "Chest_Small", new Vector3(708f, 0f, 356f)), EquipableItem.ItemList.GoldCoin }, //Random small
+                { new LocationId("blossom-house3.tmx", "Chest_Small", new Vector3(368f, 0f, 148f)), EquipableItem.ItemList.GoldCoin }, //Random small
+                { new LocationId("blossom-house4.tmx", "Chest_Small", new Vector3(696f, 0f, 416f)), EquipableItem.ItemList.GoldCoin }, //Random small
+                { new LocationId("blossom-house4.tmx", "Chest_Small", new Vector3(780f, 0f, 156f)), EquipableItem.ItemList.GoldCoin }, //Random small
+                //{ new LocationId("overworld-20x20.tmx", "Chest_Small", new Vector3(0f, 0f, 0f)), EquipableItem.ItemList.GoldCoin }, //Random small, besoin gant force
+                { new LocationId("overworld-21x20-cave.tmx", "Chest_Small", new Vector3(1192f, 0f, 612f)), EquipableItem.ItemList.GoldCoin }, //Random small
             };
         }
     }
