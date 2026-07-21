a1dbffe66f47a489ae0ce84be1c5d3b86ef625c3
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-07-20 22:07:36 -0400

    Skipper *presque* tout le texte de la cutscene pré-boss et post-boss du roi Orchid. wakeOrchid cause des erreurs lorsque copié-collée.

--- BlossomTales2.Randomizer.mm/Patchs/patch_CS_OrchidGhost.cs
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_OrchidGhost.cs
@@ -1,5 +1,6 @@
 ﻿using BlossomTales2.Randomizer.mm;
 using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 
 namespace BlossomTales2
 {
@@ -7,17 +8,50 @@ namespace BlossomTales2
     {
         private Puppet orchid = new Puppet ("Fake king", Vector3.Zero);
         private Puppet orchidLight = new Puppet ("Fake light", Vector3.Zero);
+        private Puppet orchidTomb = new Puppet("Fake tomb", Vector3.Zero);
 
         public extern void orig_giveHeart();
+
+        public void howDareYou()
+        {
+            SwitchWithBoss();
+        }
+
+        public void talkOrchid()
+        {
+            giveHeart();
+        }
 
         public void giveHeart()
         {
             GameLogger.LogInfo(Game1.CurrentLevel.Name + " " + orchid.name + " " + orchid.getPosition());
             Vector3 positionOffset = orchid.getPosition();
             positionOffset.Y = 0f; //King is floating and it screws the LocationId.
-            EquipableItem.ItemList item = RandomizerSingleton.Instance.GetItemAtLocation(new LocationId(Game1.CurrentLevel.Name, orchid.name, positionOffset));
+            EquipableItem.ItemList item = RandomizerSingleton.Instance.GetItemAtLocation(new LocationId(Game1.CurrentLevel.Name, orchid.name + "_heart", positionOffset));
             Game1.player.GiveItemReflection(item);
             tweener.Timer(3f).OnComplete(openTomb);
+        }
+
+        public void openTomb()
+        {
+            tweener.Timer(1f).OnComplete(delegate
+            {
+                Game1.Camera.Shake(16f, 0.96f);
+                orchidTomb.play("open");
+                Game1.playSoundCue("unlock_4");
+                Game1.SControl.playSounds(new List<string> { "blank103", "blank103" }, new List<int> { 200, 200 });
+                Game1.Particles.Add(new Shockwave(orchid.getPosition(), 0f, 12));
+                Game1.makeLightOrb(orchidTomb.getPosition(), 30, 1f);
+                tweener.Timer(1f).OnComplete(delegate
+                {
+                    Mod_SkipOpenTombCutscene();
+                });
+            });
+        }
+
+        private void Mod_SkipOpenTombCutscene()
+        {
+            takeSword();
         }
     }
 }
