16891c8ddf7f2ff76d1f55c01144e67c5b5fd51e
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-07-21 19:33:54 -0400

    Skipper les cutscenes du roi et ajouter une switch globale pour skip les cutscenes.

---
+++ BlossomTales2.Randomizer.mm/ModGlobals.cs
@@ -0,0 +1,8 @@
+﻿namespace BlossomTales2
+{
+    public static class ModGlobals
+    {
+        public static bool SkipCutscenes = true;
+        public static bool SkipFestival = true;
+    }
+}
--- BlossomTales2.Randomizer.mm/Patchs/patch_CS_OrchidGhost.cs
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_OrchidGhost.cs
@@ -10,30 +10,73 @@ namespace BlossomTales2
         private Puppet orchidLight = new Puppet ("Fake light", Vector3.Zero);
         private Puppet orchidTomb = new Puppet("Fake tomb", Vector3.Zero);
 
+        public extern void orig_howDareYou();
+        public extern void orig_talkOrchid();
         public extern void orig_giveHeart();
+        public extern void orig_openTomb();
 
         public void howDareYou()
         {
-            SwitchWithBoss();
+            if (ModGlobals.SkipCutscenes)
+                SwitchWithBoss();
+            else
+                orig_howDareYou();
         }
 
         public void talkOrchid()
         {
-            giveHeart();
+            if (ModGlobals.SkipCutscenes)
+                giveHeart();
+            else
+                orig_talkOrchid();
         }
 
         public void giveHeart()
         {
+            Game1.Gui.HideHud = false;
             GameLogger.LogInfo(Game1.CurrentLevel.Name + " " + orchid.name + " " + orchid.getPosition());
             Vector3 positionOffset = orchid.getPosition();
             positionOffset.Y = 0f; //King is floating and it screws the LocationId.
             EquipableItem.ItemList item = RandomizerSingleton.Instance.GetItemAtLocation(new LocationId(Game1.CurrentLevel.Name, orchid.name + "_heart", positionOffset));
             Game1.player.GiveItemReflection(item);
-            tweener.Timer(3f).OnComplete(openTomb);
+            if (ModGlobals.SkipCutscenes)
+            {
+                tweener.Timer(3f).OnComplete(delegate
+                {
+                    Game1.Gui.HideHud = true;
+                    openTomb();
+                });
+            }
+            else
+            {
+                giveHeartWithCutscene();
+            }
+        }
+
+        private void giveHeartWithCutscene()
+        {
+            tweener.Timer(3f).OnComplete(delegate
+            {
+                Game1.Gui.HideHud = true;
+                Game1.Dialoger.AddLine("<E>Old King: Thou fightest with valor and ambition not seen since my mortal days.");
+                Game1.Dialoger.AddLine("<E>Old King: But thou art strangely dressed... for a knight.");
+                Game1.Dialoger.AddLine("<E>Old King: What is thy purpose for coming here, young one?");
+                Game1.Dialoger.AddLine("Lily: I need help defeating the <A>Minotaur <A>King. He took my brother!");
+                Game1.Dialoger.AddLine("<E>Old King: Ah, the <A>Minotaur <A>King! I recall hearing stories about him when I was but a young lad.");
+                Game1.Dialoger.AddLine("<E>Old King: If the fables are true, the <A>Minotaur <A>King took <D>Chrys to a castle hidden within a labyrinth.");
+                Game1.Dialoger.AddLine("Lily: I'm not afraid! I'll rescue him, no matter where he is.");
+                Game1.Dialoger.AddLine("<E>Old King: Thy courage may ring true, but none can embark on such a journey wielding wooden weaponry.", openTomb);
+            });
         }
 
         public void openTomb()
         {
+            if(!ModGlobals.SkipCutscenes)
+            {
+                orig_openTomb();
+                return;
+            }
+
             tweener.Timer(1f).OnComplete(delegate
             {
                 Game1.Camera.Shake(16f, 0.96f);
@@ -64,7 +107,10 @@ namespace BlossomTales2
                 Game1.player.GiveItemReflection(item);
                 tweener.Timer(3f).OnComplete(delegate
                 {
-                    orchidMorkla();
+                    if (ModGlobals.SkipCutscenes)
+                        fadeOut(); //TODO: Skip la cutscene du minotaure et sortir du donjon.
+                    else
+                        orchidMorkla();
                 });
             });
         }
