25f21c2d3f01105ed15c84e72f392d880a74a8c0
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-07-25 16:15:55 -0400

    Skip la cutscene des pirates

---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_MorklaBarrier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlossomTales2
+{
+    internal class patch_CS_MorklaBarrier : CS_MorklaBarrier
+    {
+        public extern void orig_Init();
+        public extern void orig_goPirates();
+
+        public override void Init()
+        {
+            if (ModGlobals.OpenWorldState)
+                return;            
+
+            orig_Init();
+        }
+
+        public void goPirates()
+        {
+            if (ModGlobals.OpenWorldState)
+                return;
+
+            if (ModGlobals.SkipCutscenes && Game1.Globals.MainQuestObjective <= Globaler.MainGameObjective.jungles_headToTown)
+            {
+                Game1.Globals.MainQuestObjective = Globaler.MainGameObjective.jungles_getBombs;
+                return;
+            }
+
+            orig_goPirates();
+        }
+    }
+}
