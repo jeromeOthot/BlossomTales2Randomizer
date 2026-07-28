3600b19a2336f65e2960356a81da490e3bec833d
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-07-27 22:14:58 -0400

    Patcher la progression après avoir battu le capitaine des pirates.

---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_BossPirateCaptain.cs
@@ -0,0 +1,42 @@
+﻿using BlossomTales2.Extensions;
+using Microsoft.Xna.Framework;
+
+namespace BlossomTales2
+{
+    internal class patch_BossPirateCaptain : BossPirateCaptain
+    {
+        private int ExplodingTimer;
+        public extern void orig_Update(GameTime gameTime);
+        public extern void orig_Die();
+
+        public patch_BossPirateCaptain(Vector3 position) : base(position)
+        {            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Globaler.MainGameObjective currentObjective = Game1.Globals.MainQuestObjective;
+            orig_Update(gameTime);
+            if(ExplodingTimer > 100)
+            {
+                if(ModGlobals.OpenWorldState)
+                {
+                    Game1.Globals.MainQuestObjective = currentObjective;
+                    Game1Extensions.MarkObjectiveComplete(Globaler.MainGameObjective.jungles_morklaEnter);
+                }
+            }
+        }
+
+        public override void Die()
+        {
+            Globaler.MainGameObjective currentObjective = Game1.Globals.MainQuestObjective;
+            orig_Die();
+
+            if (ModGlobals.OpenWorldState)
+            {
+                Game1.Globals.MainQuestObjective = currentObjective;
+                Game1Extensions.MarkObjectiveComplete(Globaler.MainGameObjective.jungles_morklaEnter);
+            }
+        }
+    }
+}
---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_PirateCaptain.cs
@@ -0,0 +1,53 @@
+﻿using BlossomTales2.Extensions;
+using Microsoft.Xna.Framework;
+
+namespace BlossomTales2
+{
+    internal class patch_CS_PirateCaptain : CS_PirateCaptain
+    {
+        private bool inited;
+        private Puppet captain;
+
+        public extern void orig_Init();
+
+        public override void Init()
+        {
+            if (Mod_IsPirateDefeated())
+            {
+                inited = true;
+                Chest chest = new Chest(new Vector3(Game1.CurrentLevel.Width * 32, 0f, 640f));
+                chest.IDNumber = 11;
+                if (Mod_HasOpenedChest())
+                {
+                    chest.Frame = 7;
+                }
+                else
+                {
+                    Game1.WaterLevelUp = false;
+                }
+
+                Game1.CurrentLevel.LevelObjects.Add(chest);
+            }
+            else
+            {
+                captain = new Puppet("pirateCaptain", new Vector3(Game1.CurrentLevel.Width * 32, 0f, 256f));
+                captain.play("swimming");
+                captain.Alpha = 0f;
+                puppets.Add(captain);
+            }
+        }
+
+        private bool Mod_IsPirateDefeated()
+        {
+            if (ModGlobals.OpenWorldState)
+                return Game1Extensions.IsObjectiveCompleted(Globaler.MainGameObjective.jungles_morklaEnter);
+            else
+                return Game1.Globals.MainQuestObjective >= Globaler.MainGameObjective.jungles_pirateDefeated;
+        }
+
+        private bool Mod_HasOpenedChest()
+        {
+            return Game1Extensions.HasLevelPermaObject("Chest");
+        }
+    }
+}
