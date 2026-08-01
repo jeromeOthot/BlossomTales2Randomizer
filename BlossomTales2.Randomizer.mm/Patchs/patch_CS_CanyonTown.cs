76a34fc4cece9407ea1ffa83185de3da6269b8f4
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-08-01 16:40:40 -0400

    Skip la cutscene dans le village canyon.

---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_CanyonTown.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace BlossomTales2
+{
+    internal class patch_CS_CanyonTown : CS_CanyonTown
+    {
+        private Puppet beggar;
+
+        public extern void orig_Init();
+
+        public override void Init()
+        {
+            if (ModGlobals.OpenWorldState)
+            {
+                if (Game1.Globals.beggarState < 5)
+                {
+                    beggar = new Puppet("beggar", new Vector3(980f, 0f, 1616f));
+                    beggar.play("sit");
+                    beggar.collide = true;
+                    beggar.isNPC = true;
+                    beggar.DialogNum = 163;
+                    puppets.Add(beggar);
+                    puppetList.Add(beggar);
+                }
+                return;
+            }
+            else
+            {
+                orig_Init();
+            }
+        }
+    }
+}
