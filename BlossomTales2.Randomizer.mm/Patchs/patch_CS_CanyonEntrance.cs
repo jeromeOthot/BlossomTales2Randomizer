1075c7b95851822f37d556b352c8756987e68e2f
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-08-01 15:36:34 -0400

    Skipper la cutscene du hibou car c'est juste des informations.

---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_CanyonEntrance.cs
@@ -0,0 +1,15 @@
+﻿namespace BlossomTales2
+{
+    internal class patch_CS_CanyonEntrance : CS_CanyonEntrance
+    {
+        public extern void orig_goOwl();
+
+        public void goOwl()
+        {
+            if(ModGlobals.OpenWorldState)
+                return;
+            else
+                orig_goOwl();
+        }
+    }
+}
