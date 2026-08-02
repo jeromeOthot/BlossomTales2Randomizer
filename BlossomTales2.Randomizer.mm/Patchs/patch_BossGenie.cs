3192632490e9a0943c6720fef35aec9e6dd78b8c
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-08-02 14:14:44 -0400

    Patch la progression et les items du boss Genie

---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_BossGenie.cs
@@ -0,0 +1,62 @@
+﻿using BlossomTales2.Randomizer.mm;
+using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoMod;
+using MonoMod.Cil;
+using MonoMod.InlineRT;
+using MonoMod.Utils;
+using System;
+
+namespace BlossomTales2
+{
+    public class patch_BossGenie : BossGenie
+    {
+        public patch_BossGenie(Vector3 position) : base(position)
+        {
+        }
+
+        [MonoModIgnore]
+        [PatchBossGenieUpdate]
+        public extern void Update(GameTime gameTime);
+    }
+
+    public class ModBossGenie
+    {
+        public static void Mod_GiveHeart(BossGenie bossGenie)
+        {
+            EquipableItem.ItemList item = RandomizerSingleton.Instance.GetItemAtLocation(new LocationId(Game1.CurrentLevel.Name, bossGenie.Name, Vector3.Zero));
+            Game1.player.GiveItemReflection(item);
+        }
+    }
+}
+
+namespace MonoMod
+{
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchBossGenieUpdate))]
+    class PatchBossGenieUpdateAttribute : Attribute { }
+
+    static partial class MonoModRules
+    {
+        public static void PatchBossGenieUpdate(ILContext context, CustomAttribute attrib)
+        {
+            TypeDefinition modBossGenieType = MonoModRule.Modder.FindType("BlossomTales2.ModBossGenie").Resolve();
+            MethodDefinition mod_GiveHeartMethod = modBossGenieType.FindMethod("Mod_GiveHeart");
+
+            ILCursor cursor = new ILCursor(context);
+            //Find
+            //Game1.player.GiveItem(EquipableItem.ItemList.HeartQ_4);
+            cursor.GotoNext(MoveType.Before,
+                instr => instr.MatchLdsfld("BlossomTales2.Game1", "player"),
+                instr => instr.MatchLdcI4(27),
+                instr => instr.MatchLdcI4(1),
+                instr => instr.MatchCallvirt("BlossomTales2.Player", "GiveItem")
+            );
+            //Replace with
+            //ModBossGenie.Mod_GiveHeart(this)
+            cursor.RemoveRange(4);
+            cursor.Emit(OpCodes.Ldarg_0);
+            cursor.Emit(OpCodes.Call, mod_GiveHeartMethod);
+        }
+    }
+}
---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_GenieBoss.cs
@@ -0,0 +1,75 @@
+﻿using BlossomTales2.Extensions;
+using Microsoft.Xna.Framework;
+
+namespace BlossomTales2
+{
+    public class patch_CS_GenieBoss : CS_GenieBoss
+    {
+        public extern void orig_changeLevel();
+
+        private Vector3 startpos;
+
+        public override void Init()
+        {
+            if (Game1.Globals.Portal_Temple < 3)
+            {
+                Game1.Globals.Portal_Temple = 3;
+            }
+            if (Mod_HasNotDefeatedGenie() && !Game1.Globals.playedGenieIntro && !Game1.Globals.Def_BossDjinn)
+            {
+                focusCameraOnTarget(new Vector2(Game1.CurrentLevel.Width * 32, 576f), 2f, littleTalk);
+            }
+            Chest chest = new Chest(startpos);
+            if (!Game1.Globals.Def_BossDjinn)
+            {
+                chest.IDNumber = 101;
+            }
+            else if (!Mod_HasOpenedBossChest())
+            {
+                chest.IDNumber = 22;
+            }
+            else
+            {
+                chest.Frame = 7;
+            }
+            Game1.CurrentLevel.LevelObjects.Add(chest);
+            foreach (LevelObject levelObject in Game1.CurrentLevel.LevelObjects)
+            {
+                if (levelObject is CameraOverrider)
+                {
+                    levelObject.Position.X += 2000f;
+                }
+                if (levelObject is RaisingWall)
+                {
+                    levelObject.Size.Y = 0f;
+                    levelObject.Frame = 0;
+                }
+            }
+        }
+
+        public void changeLevel()
+        {
+            Globaler.MainGameObjective mainGameObjective = Game1.Globals.MainQuestObjective;
+            orig_changeLevel();
+
+            if (ModGlobals.OpenWorldState)
+            {
+                Game1Extensions.MarkObjectiveComplete(Globaler.MainGameObjective.canyons_fightGolem);
+                Game1.Globals.MainQuestObjective = mainGameObjective;
+            }
+        }
+
+        private bool Mod_HasNotDefeatedGenie()
+        {
+            if (ModGlobals.OpenWorldState)
+                return !Game1Extensions.IsObjectiveCompleted(Globaler.MainGameObjective.canyons_fightGolem);
+            else
+                return Game1.Globals.MainQuestObjective <= Globaler.MainGameObjective.canyons_fightGolem;
+        }
+
+        private bool Mod_HasOpenedBossChest()
+        {
+            return Game1Extensions.HasLevelPermaObject("Chest");
+        }
+    }
+}
