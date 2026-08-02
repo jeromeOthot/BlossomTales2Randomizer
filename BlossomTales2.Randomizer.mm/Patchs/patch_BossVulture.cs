0fcb259ec0c7b4223ae6052b6d2e20172cef4a9a
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-08-02 11:12:40 -0400

    Patch le miniboss Vulture du temple

---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_BossVulture.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using BlossomTales2.Extensions;
+using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoMod;
+using MonoMod.Cil;
+using MonoMod.InlineRT;
+using MonoMod.Utils;
+
+namespace BlossomTales2
+{
+    internal class patch_BossVulture : BossVulture
+    {
+        public patch_BossVulture(Vector3 position) : base(position)
+        {
+        }
+
+        [MonoModIgnore]
+        [PatchBossVultureUpdate]
+        public extern void Update(GameTime gameTime);
+    }
+
+    public class ModBossVulture
+    {
+        public static void Mod_CompleteVultureObjective()
+        {
+            if (ModGlobals.OpenWorldState)
+            {
+                Game1Extensions.MarkObjectiveComplete(Globaler.MainGameObjective.canyons_headToVulture);
+            }
+            else
+            {
+                if (Game1.Globals.MainQuestObjective < Globaler.MainGameObjective.canyons_headToGolem)
+                {
+                    Game1.Globals.MainQuestObjective = Globaler.MainGameObjective.canyons_headToGolem;
+                }
+            }
+        }
+    }
+}
+
+namespace MonoMod
+{
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchBossVultureUpdate))]
+    class PatchBossVultureUpdateAttribute : Attribute { }
+
+    static partial class MonoModRules
+    {
+        public static void PatchBossVultureUpdate(ILContext context, CustomAttribute attrib)
+        {
+            TypeDefinition modBossVultureType = MonoModRule.Modder.FindType("BlossomTales2.ModBossVulture").Resolve();
+            MethodDefinition mod_CompleteVultureObjectiveMethod = modBossVultureType.FindMethod("Mod_CompleteVultureObjective");
+
+            ILCursor cursor = new ILCursor(context);
+            //Find
+            //if (Game1.Globals.MainQuestObjective < Globaler.MainGameObjective.canyons_headToGolem)
+            cursor.GotoNext(MoveType.Before,
+                instr => instr.MatchLdsfld("BlossomTales2.Game1", "Globals"),
+                instr => instr.MatchLdfld("BlossomTales2.Globaler", "MainQuestObjective"),
+                instr => instr.MatchLdcI4(28),
+                instr => instr.MatchBge(out ILLabel label)
+            );
+            //Replace with
+            //ModBossVulture.Mod_CompleteVultureObjective()
+            cursor.RemoveRange(7);
+            cursor.Emit(OpCodes.Call, mod_CompleteVultureObjectiveMethod);
+        }
+    }
+}
---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_VultureBoss.cs
@@ -0,0 +1,63 @@
+﻿using BlossomTales2.Extensions;
+using Microsoft.Xna.Framework;
+
+namespace BlossomTales2
+{
+    public class patch_CS_VultureBoss : CS_VultureBoss
+    {
+        private bool inited;
+
+        public extern void orig_Init();
+
+        public override void Init()
+        {
+            if (Game1.Globals.Portal_Temple < 2)
+            {
+                Game1.Globals.Portal_Temple = 2;
+            }
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(200f, 0f, 555f), reg: false, 1.5f, 1));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(555f, 0f, 500f), reg: false, 1.5f, 0));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(1000f, 0f, 500f), reg: false, 1.5f, 1));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(1400f, 0f, 555f), reg: false, 1.5f, 1));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(1700f, 0f, 500f), reg: false, 1.5f, 0));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(2100f, 0f, 555f), reg: false, 1.5f, 1));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(200f, 0f, 1500f), reg: false, 2000f, 1));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(650f, 0f, 1580f), reg: false, 2000f, 0));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(1100f, 0f, 1580f), reg: false, 2000f, 1));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(1400f, 0f, 1500f), reg: false, 2000f, 1));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(1700f, 0f, 1530f), reg: false, 2000f, 0));
+            Game1.CurrentLevel.LevelObjects.Add(new Cloud(new Vector3(2100f, 0f, 1500f), reg: false, 2000f, 1));
+            foreach (LevelObject levelObject in Game1.CurrentLevel.LevelObjects)
+            {
+                if (levelObject is RaisingWall)
+                {
+                    levelObject.Size.Y = 0f;
+                }
+            }
+            if (Mod_HasCompletedVultureObjective())
+            {
+                inited = true;
+                Chest chest = new Chest(new Vector3(Game1.CurrentLevel.Width * 32, 0f, 640f));
+                chest.IDNumber = 12;
+                if (Mod_HasOpenedChest())
+                {
+                    chest.Frame = 7;
+                }
+                Game1.CurrentLevel.LevelObjects.Add(chest);
+            }
+        }
+
+        private bool Mod_HasCompletedVultureObjective()
+        {
+            if (ModGlobals.OpenWorldState)
+                return Game1Extensions.IsObjectiveCompleted(Globaler.MainGameObjective.canyons_headToVulture);
+            else
+                return Game1.Globals.MainQuestObjective >= Globaler.MainGameObjective.canyons_headToGolem;
+        }
+
+        private bool Mod_HasOpenedChest()
+        {
+            return Game1Extensions.HasLevelPermaObject("Chest");
+        }
+    }
+}
