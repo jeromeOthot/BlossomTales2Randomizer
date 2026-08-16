5ed5b780991f39c38c28bc1590e2e277d4e8abe9
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-08-16 18:08:39 -0400

    Fix le chest du boss 1

--- BlossomTales2.Randomizer.mm/Patchs/patch_CS_OctopusBoss.cs
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_OctopusBoss.cs
@@ -1,10 +1,35 @@
 ﻿using BlossomTales2.Extensions;
+using Microsoft.Xna.Framework;
 
 namespace BlossomTales2
 {
-    internal class patch_CS_OctopusBoss :CS_OctopusBoss
+    internal class patch_CS_OctopusBoss : CS_OctopusBoss
     {
         public extern void orig_switchMaps();
+
+        public override void Init()
+        {
+            if (!Game1.Globals.Def_BossOctopus)
+            {
+                Game1.SwitchBGMusic("stop", 0.01f);
+                Game1.Gui.HideHud = true;
+                Game1.player.RemovePlayerControls = true;
+                Game1.player.EnteringDoor = false;
+                Game1.player.EnteringDoorAmount = 0;
+                focusCameraOnTarget(new Vector2(Game1.CurrentLevel.Width * 32, 640f), 2.5f, shutDoor);
+                Game1.player.MoveToPosition(new Vector3(Game1.CurrentLevel.Width * 32, 0f, 768f), 1);
+            }
+            else
+            {
+                Chest chest = new Chest(new Vector3(Game1.CurrentLevel.Width * 32, 0f, Game1.CurrentLevel.Height * 32));
+                chest.IDNumber = 21;
+                if (Mod_HasOpenedBossChest())
+                {
+                    chest.Frame = 7;
+                }
+                Game1.CurrentLevel.LevelObjects.Add(chest);
+            }
+        }
 
         internal void switchMaps()
         {
@@ -21,6 +46,11 @@ namespace BlossomTales2
                 Game1Extensions.MarkObjectiveComplete(Globaler.MainGameObjective.jungles_morklaGetEyes);
             else
                 Game1.Globals.MainQuestObjective = Globaler.MainGameObjective.jungles_morklaComplete;
+        }
+
+        private bool Mod_HasOpenedBossChest()
+        {
+            return Game1Extensions.HasLevelPermaObject("Chest");
         }
     }
 }
