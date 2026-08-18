85f97f3e2086076a15926bf92d90ca99dc654976
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-08-17 21:49:57 -0400

    Patcher la side quest de slime dans la ville labyrinthe

---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_LabSlime.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using BlossomTales2.Extensions;
+using BlossomTales2.Randomizer.mm;
+using Microsoft.Xna.Framework;
+
+namespace BlossomTales2
+{
+    public class patch_CS_LabSlime : CS_LabSlime
+    {
+        public void getHeart()
+        {
+            RandomizerSingleton.Instance.GiveItemAtLocation("labSlime", Vector3.Zero);
+            Game1.Globals.labSlimeState = 2;
+        }
+    }
+}
