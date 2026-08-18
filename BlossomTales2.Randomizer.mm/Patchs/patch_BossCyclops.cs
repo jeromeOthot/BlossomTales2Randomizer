3d4649ef55942ed3a2a6b670a13607a2e5b6799b
Marc-André Boucher <butchlecolosse@hotmail.com>
2026-08-17 21:49:40 -0400

    Patcher le boss cyclops

---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_BossCyclops.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BlossomTales2.Extensions;
+using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoMod;
+using MonoMod.Cil;
+using MonoMod.InlineRT;
+using MonoMod.Utils;
+
+namespace BlossomTales2
+{
+    public class patch_BossCyclops : BossCyclops
+    {
+        public patch_BossCyclops(Vector3 position) : base(position)
+        {
+        }
+        [MonoModIgnore]
+        [PatchBossCyclopsUpdate]
+        public extern void Update(GameTime gameTime);
+    }
+
+    public class ModBossCyclops
+    {
+        public static void Mod_CompleteCyclopsObjective()
+        {
+            if (ModGlobals.OpenWorldState)
+            {
+                Game1Extensions.MarkObjectiveComplete(Globaler.MainGameObjective.lab_enterLabyrinth);
+            }
+            else
+            {
+                if (Game1.Globals.MainQuestObjective < Globaler.MainGameObjective.lab_getMirrorShield)
+                {
+                    Game1.Globals.MainQuestObjective = Globaler.MainGameObjective.lab_getMirrorShield;
+                }
+            }
+        }
+    }
+}
+
+namespace MonoMod
+{
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchBossCyclopsUpdate))]
+    class PatchBossCyclopsUpdateAttribute : Attribute { }
+
+    static partial class MonoModRules
+    {
+        public static void PatchBossCyclopsUpdate(ILContext context, CustomAttribute attrib)
+        {
+            TypeDefinition modBossCyclopsType = MonoModRule.Modder.FindType("BlossomTales2.ModBossCyclops").Resolve();
+            MethodDefinition mod_CompleteCyclopsObjectiveMethod = modBossCyclopsType.FindMethod("Mod_CompleteCyclopsObjective");
+
+            ILCursor cursor = new ILCursor(context);
+            //Find
+            //if (Game1.Globals.MainQuestObjective < Globaler.MainGameObjective.lab_getMirrorShield)
+            cursor.GotoNext(MoveType.Before,
+                instr => instr.MatchLdsfld("BlossomTales2.Game1", "Globals"),
+                instr => instr.MatchLdfld("BlossomTales2.Globaler", "MainQuestObjective"),
+                instr => instr.MatchLdcI4(49),
+                instr => instr.MatchBge(out ILLabel label)
+            );
+            //Replace with
+            //ModBossCyclops.Mod_CompleteCyclopsObjective()
+            cursor.RemoveRange(7);
+            cursor.Emit(OpCodes.Call, mod_CompleteCyclopsObjectiveMethod);
+        }
+    }
+}
---
+++ BlossomTales2.Randomizer.mm/Patchs/patch_CS_BossCyclops.cs
@@ -0,0 +1,66 @@
+﻿using BlossomTales2.Extensions;
+using Microsoft.Xna.Framework;
+
+namespace BlossomTales2
+{
+    public class patch_CS_BossCyclops : CS_BossCyclops
+    {
+        private bool inited;
+
+        public override void Init()
+        {
+            Game1.CurrentLevel.LevelObjects.Add(new MirrorPost(new Vector3(1504f, 0f, 1312f)));
+            Game1.CurrentLevel.LevelObjects.Add(new MirrorPost(new Vector3(1504f, 0f, 1632f)));
+            Game1.CurrentLevel.LevelObjects.Add(new MirrorPost(new Vector3(2208f, 0f, 1312f)));
+            Game1.CurrentLevel.LevelObjects.Add(new MirrorPost(new Vector3(2208f, 0f, 1632f)));
+            if (Mod_HasNotDefeatedCyclops())
+            {
+                foreach (LevelObject levelObject in Game1.CurrentLevel.LevelObjects)
+                {
+                    if (levelObject is CameraOverrider)
+                    {
+                        levelObject.Position.X += 2000f;
+                    }
+                    else if (levelObject is RaisingWall && levelObject.IDNumber == 9)
+                    {
+                        levelObject.Size.Y = 0f;
+                    }
+                }
+                return;
+            }
+            Chest chest = new Chest(new Vector3(1856f, 0f, 1408f));
+            chest.IDNumber = 14;
+            if (Mod_HasOpenedChest())
+            {
+                chest.Frame = 7;
+            }
+            Game1.CurrentLevel.LevelObjects.Add(chest);
+            foreach (LevelObject levelObject2 in Game1.CurrentLevel.LevelObjects)
+            {
+                if (levelObject2 is CameraOverrider)
+                {
+                    levelObject2.Alive = false;
+                    break;
+                }
+                if (levelObject2 is RaisingWall && levelObject2.IDNumber == 9)
+                {
+                    levelObject2.Size.Y = 0f;
+                }
+            }
+            inited = true;
+        }
+
+        private bool Mod_HasNotDefeatedCyclops()
+        {
+            if (ModGlobals.OpenWorldState)
+                return !Game1Extensions.IsObjectiveCompleted(Globaler.MainGameObjective.lab_enterLabyrinth);
+            else
+                return Game1.Globals.MainQuestObjective <= Globaler.MainGameObjective.lab_enterLabyrinth;
+        }
+
+        private bool Mod_HasOpenedChest()
+        {
+            return Game1Extensions.HasLevelPermaObject("Chest");
+        }
+    }
+}
