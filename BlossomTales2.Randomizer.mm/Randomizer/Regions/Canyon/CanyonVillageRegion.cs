c36ca3c509feb2b53ebca5dd6bce3808f96b8c88
jeromeOthot <jerome.othot@gmail.com>
2026-09-19 12:24:05 -0400

    Correction region Canyon Village

--- BlossomTales2.Randomizer.mm/Randomizer/Regions/Canyon/CanyonVillageRegion.cs
+++ BlossomTales2.Randomizer.mm/Randomizer/Regions/Canyon/CanyonVillageRegion.cs
@@ -21,6 +21,11 @@ namespace BlossomTales2.Randomizer.mm.Canyon
                 { new Location( new LocationId("canyon-shop.tmx", "canyon-shop left", Vector3.Zero), "canyon-shop  left",(inventory) => true, ItemType.Jar_Empty) },
                 { new Location(new LocationId("canyon-shop.tmx", "canyon-shop center", Vector3.Zero), "canyon-shop center ",(inventory) => true, ItemType.Crystal) },
                 { new Location(new LocationId("canyon-shop.tmx", "canyon-shop right", Vector3.Zero), "canyon-shop right",(inventory) => true, ItemType.HeartQ_1) },
+
+                //House chest
+                 { new Location(new LocationId("canyon-house1.tmx", "Chest_Small", new Vector3(576f, 0f, 208f)), "canyon village house 1 chest #1",(inventory) => true, ItemType.GoldCoin) }, //accès canyon
+                 { new Location(new LocationId("canyon-house3.tmx", "Chest_Small", new Vector3(772f, 0f, 580f)), "canyon village house 3 chest #1",(inventory) => true, ItemType.GoldCoin) }, //accès canyon
+                 { new Location(new LocationId("canyon-house3.tmx", "Chest_Small", new Vector3(344f, 0f, 496f)), "canyon village house 3 chest #2",(inventory) => true, ItemType.GoldCoin) }, //accès canyon
             };
 
         }
